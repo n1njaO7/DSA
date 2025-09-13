902179728f2faa6da9b33309ee72fc3327d4fbaa
n1njaO7 <n1njasri07@gmail.com>
2025-09-13 22:02:53 +0530

    created insertAtStart function

--- Array/SinglyLinkedListDataStructure.cpp
+++ Array/SinglyLinkedListDataStructure.cpp
@@ -12,15 +12,25 @@ class SLL{
         node *start;
     public:
         SLL();
+        SLL(SLL&);
+        SLL& operator=(SLL&);
         void insertAtStart(int);    
         void insertAtLast(int);    
         void insertAfter(node*,int);
-        node* search(int);    
+        node* search(int); 
+        void deleteFirst();   
+        void deleteLast();   
+        void deleteNode(node*);  
+        ~SLL(); 
 };
 
 SLL::SLL(){
     start = NULL;
 }
 void SLL :: insertAtStart(int data){
-    
+    node *n=new node;
+    n->item = data;
+    n->next = start;
+    start = n;
 }
+
