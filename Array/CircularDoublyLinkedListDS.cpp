361c2889d48e61654a07da69004d897c6011fe6a
n1njaO7 <n1njasri07@gmail.com>
2025-09-19 20:42:28 +0530

    created class and declared function

---
+++ Array/CircularDoublyLinkedListDS.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+struct node{
+    int item;
+    node* next;
+    node* prev;
+};
+
+class CDLL{
+    private:
+        node *start;
+    public:
+        CDLL();
+        CDLL(CDLL&);
+        CDLL& operator=(CDLL&);
+        void insertAtStart(int);    
+        void insertAtLast(int);    
+        void insertAfter(node*,int);
+        node* search(int); 
+        void deleteFirst();   
+        void deleteLast();   
+        void deleteNode(node*);  
+        ~CDLL(); 
+};
