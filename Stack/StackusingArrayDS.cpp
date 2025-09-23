326cbea913e3651901d490d03d4f9efaf8126361
n1njaO7 <n1njasri07@gmail.com>
2025-09-23 15:29:56 +0530

    created class and fun of stack

---
+++ Stack/StackusingArrayDS.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Stack
+{
+    private :
+        int capacity;
+        int top;
+        int *ptr;
+    public:
+        Stack();
+        Stack(Stack&);
+        Stack &operator = (Stack&); 
+        void createStack(int);
+        void insert(int,int);
+        void append(int);
+        int getItem(int);
+        bool isEmpty();
+        bool isFull();
+        void del(int);
+        void edit(int,int);
+        int count();
+        int getCapacity();
+        ~Stack();
+};
